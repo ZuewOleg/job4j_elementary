475ee2edaf515092292fd4159bb072ff4d535d2b
ZuewOleg <zuev_17@bk.ru>
2020-11-24 22:58:44 +0500

    9.1. String [#413980]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main (String[] args) {
+    String id = "I like Java!" + "But I am a newbie.";
+          int year = 2020;
+        String idea = id + year;
+        System.out.println(idea);
+    }
+}
