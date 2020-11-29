65609d3de8de68d369c247606711e8bfd5b8f7f2
ZuewOleg <zuev_17@bk.ru>
2020-11-29 16:48:35 +0500

    4.3.3. Логическое отрицание ! [#413928]

---
+++ src/ru/job4j/condition/LogicNot.java
@@ -0,0 +1,37 @@
+package ru.job4j.condition;
+
+public class LogicNot {
+    public static boolean isEven(int num){
+        return num % 2 == 0;
+    }
+    public static boolean isPositive(int num){
+        return num > 0;
+    }
+    public static boolean notEven(int num){
+        return !isEven(num);
+    }
+    public static boolean notPositive(int num){
+        return !isPositive(num);
+    }
+    public static boolean notEvenAndPositive(int num){
+        return notEven(num) && isPositive(num);
+    }
+    public static boolean evenOrNotPositive(int num){
+        return isEven(num) && notPositive(num);
+    }
+
+    public static void main(String[] args) {
+        boolean result1 = LogicNot.isEven(2);
+        System.out.println(result1);
+        boolean result2 = LogicNot.isPositive(-2);
+        System.out.println(result2);
+        boolean result3 = LogicNot.notEven(7);
+        System.out.println(result3);
+        boolean result4 = LogicNot.notPositive(4);
+        System.out.println(result4);
+        boolean result5 = LogicNot.notEvenAndPositive(1);
+        System.out.println(result5);
+        boolean result6 = LogicNot.evenOrNotPositive(-2);
+        System.out.println(result6);
+    }
+}
---
+++ test/ru/job4j/condition/LogicNotTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class LogicNotTest {
+
+    @Test
+    public void isEven() {
+    }
+
+    @Test
+    public void isPositive() {
+    }
+
+    @Test
+    public void notEven() {
+    }
+
+    @Test
+    public void notPositive() {
+    }
+
+    @Test
+    public void notEvenAndPositive() {
+    }
+
+    @Test
+    public void evenOrNotPositive() {
+    }
+
+    @Test
+    public void testIsEven() {
+    }
+
+    @Test
+    public void testIsPositive() {
+    }
+
+    @Test
+    public void testNotEven() {
+    }
+
+    @Test
+    public void testNotPositive() {
+    }
+
+    @Test
+    public void testNotEvenAndPositive() {
+    }
+
+    @Test
+    public void testEvenOrNotPositive() {
+    }
+
+    @Test
+    public void main() {
+    }
+}
