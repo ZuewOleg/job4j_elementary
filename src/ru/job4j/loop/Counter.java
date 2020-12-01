42866ebc752f3f662cce2682a723fe5062bb47bd
ZuewOleg <zuev_17@bk.ru>
2020-12-01 10:49:20 +0500

    5.0.1. Сумма чисел. [#414032]

---
+++ src/ru/job4j/loop/Counter.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Counter {
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        for (int a = start; a <= finish; a++) {
+            sum = sum + a;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(5, 7));
+        System.out.println(sum(1, 12));
+        System.out.println(sum(0, 3));
+    }
+}
