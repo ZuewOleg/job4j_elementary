a0bfebac5176096a9207726ae429c23344d7a9c3
ZuewOleg <zuev_17@bk.ru>
2020-12-07 10:51:34 +0500

    5.7. Ипотека [#414029]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,10 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, double percent) {
         int year = 0;
-        double credit = amount;
-        while (credit >= 0) {
-            credit = credit + (credit * (percent / 100));
-            credit -= salary;
+        while (amount >= 0) {
+            amount = (int) (amount + (amount * (percent / 100)));
+            amount -= salary;
             year++;
         }
         return year;
