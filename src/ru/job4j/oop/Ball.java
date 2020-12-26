bfcba4f8766a4fbf9e8b472cf7bae78739ae23a7
ZuewOleg <zuev_17@bk.ru>
2020-12-26 16:22:03 +0500

    1.6. Взаимодействие объектов [#414134]

---
+++ src/ru/job4j/oop/Ball.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void runHare(Hare hare) {
+    }
+
+    public void runWolf(Wolf wolf) {
+    }
+
+    public void runFox(Fox fox) {
+    }
+}
