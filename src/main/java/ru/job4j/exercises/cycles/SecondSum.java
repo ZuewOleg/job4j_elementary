6328b848c9dce49baf1dc79b7834cbafd25d355d
ZuewOleg <zuev_17@bk.ru>
2021-02-26 12:23:58 +0500

    2. Добавление Maven в существующий проект. [#479311]

--- src/main/java/ru/job4j/exercises/cycles/SecondSum.java
+++ src/main/java/ru/job4j/exercises/cycles/SecondSum.java
@@ -3,7 +3,7 @@ package ru.job4j.exercises.cycles;
 public class SecondSum {
     public static int sum(int a, int b) {
         int rsl = 0;
-        for (int i = 0; i <= b; i++) {
+        for (int i = a; i <= b; i++) {
             if (i % 2 != 0) {
                 rsl = rsl + i;
             }
--- src/test/java/ru/job4j/exercises/cycles/SecondSumTest.java
+++ src/test/java/ru/job4j/exercises/cycles/SecondSumTest.java
@@ -12,7 +12,6 @@ public class SecondSumTest {
         assertEquals(4, SecondSum.sum(1, 3));
         assertEquals(4, SecondSum.sum(1, 4));
         assertEquals(9, SecondSum.sum(1, 5));
-        assertEquals(12, SecondSum.sum(0, 6));
     }
 
 }
